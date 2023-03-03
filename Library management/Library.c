0698abbf7a6461088b93a14af77c7cb7aaf175b5
Imad Eddine Kattoussi <imadeeddinekattoussi@gmail.com>
2023-03-03 02:36:13 +0100

    library 0.1

---
+++ Library management/Library.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+{
+    printf("----------Main Menu---------\n\n");
+
+    printf("1. Add Books\n");
+    printf("2. Display Book information\n");
+    printf("3. Search Books(Book status)\n");
+    printf("4. Exit\n\n");
+
+    int a1;
+    printf("Enter : ");
+    scanf("%d", &a1);
+    printf("\n\n");
+
+    if(a1==1)
+    {
+        printf("You can add book information\n\n");
+
+        printf("Categories : \n\n");
+
+        printf("1. Math\n");
+        printf("2. Physics\n");
+        printf("3. Computer\n\n");
+
+        int a2;
+        printf("Choose a category");
+        scanf("%d", &a2);
+
+        
+
+
+    }
+
+}
